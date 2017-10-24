ecd49ebedf3b6bfa1350200ab9139a30670fbc7b
Juan Manuel Hidalgo Navarro <jmhidalgona@gmail.com>
2017-10-24 17:06:49 +0200

    Añadido forma de obtener la localización y dirección de una entidad al registrar

--- Smart Accesibility City App/app/src/main/java/sacapp/android/jmhidalgo/smartaccesibilitycityapp/activitiy/EntityRegisterActivity.java
+++ Smart Accesibility City App/app/src/main/java/sacapp/android/jmhidalgo/smartaccesibilitycityapp/activitiy/EntityRegisterActivity.java
@@ -13,11 +13,21 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
@@ -31,7 +41,9 @@ import sacapp.android.jmhidalgo.smartaccesibilitycityapp.accessdb.API;
 import sacapp.android.jmhidalgo.smartaccesibilitycityapp.accessdb.service.EntityService;
 import sacapp.android.jmhidalgo.smartaccesibilitycityapp.model.Entity;
 
-public class EntityRegisterActivity extends AppCompatActivity implements LocationListener {
+public class EntityRegisterActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
 
     private Button buttonAccept;
     private Button buttonUbication;
@@ -41,79 +53,38 @@ public class EntityRegisterActivity extends AppCompatActivity implements Locatio
     private EditText editTextEmail;
     private EditText editTextPass;
     private EditText editTextPassConfirmed;
-    private EditText editTextAddress;
+    private TextView textViewAddress;
     private EditText editTextPassWebsite;
 
     private Location entityLocation;
     private LocationManager locationManager;
 
     private Geocoder geocoder;
+    private Marker entityMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entity_register);
-
-        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
-        final Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    ACCESS_LOCATION);
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                    ACCESS_LOCATION);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                new AlertDialog.Builder(EntityRegisterActivity.this).setTitle("GPS").setMessage("Si no acepta los permisos requeridos, no puede hacer uso de la aplicación");
-                return;
-            }
-        }
-
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 10, this);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 10, this);
-
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         buttonAccept = (Button) findViewById(R.id.buttonAccept);
-        buttonUbication = (Button) findViewById(R.id.buttonUbicacion);
+
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
-        editTextAddress = (EditText) findViewById(R.id.editTextAddress);
+        textViewAddress = (TextView) findViewById(R.id.textViewAddress);
         editTextName = (EditText) findViewById(R.id.editTextName);
         editTextPassConfirmed = (EditText) findViewById(R.id.editTextPassConfirmed);
         editTextPass = (EditText) findViewById(R.id.editTextPass);
         editTextPassWebsite = (EditText) findViewById(R.id.editTextWebsite);
-
-        buttonUbication.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getLocationEntity();
-                List<Address> addresses = null;
-
-                try {
-                    addresses = geocoder.getFromLocation(entityLocation.getLatitude(), entityLocation.getLongitude(), 1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                if(addresses.isEmpty()){
-                    new AlertDialog.Builder(EntityRegisterActivity.this)
-                            .setTitle("Dirección")
-                            .setMessage("No se ha podido obtener la dirección de la entidad")
-                            .setPositiveButton("OK", null)
-                            .show();
-                } else {
-                    editTextAddress.setText(addresses.get(0).getAddressLine(0));
-                }
-            }
-        });
-
 
         buttonAccept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 String name = editTextName.getText().toString();
-                String address = editTextAddress.getText().toString();
+                String address = textViewAddress.getText().toString();
                 String pass1 = editTextPass.getText().toString();
                 String pass2 = editTextPassConfirmed.getText().toString();
                 String email = editTextEmail.getText().toString();
@@ -124,7 +95,6 @@ public class EntityRegisterActivity extends AppCompatActivity implements Locatio
                     editTextPass.requestFocus();
                     return;
                 }
-                // TODO GET LOCATION
                 Entity entity = new Entity("", name, email, pass1, "ROLE_ENTITY", "", address, entityLocation.getLongitude(), entityLocation.getLatitude(), website);
 
                 EntityService entityService = API.getApi().create(EntityService.class);
@@ -216,4 +186,134 @@ public class EntityRegisterActivity extends AppCompatActivity implements Locatio
     public void onProviderDisabled(String s) {
 
     }
+
+    // OnMapReadyCallback Implementation
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        final Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_LOCATION);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, ACCESS_LOCATION);
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                new AlertDialog.Builder(EntityRegisterActivity.this).setTitle("GPS").setMessage("Si no acepta los permisos requeridos, no puede hacer uso de la aplicación");
+                return ;
+            }
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 10, this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 10, this);
+        entityMarker = null;
+
+        getLocationEntity();
+        entityMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(entityLocation.getLatitude(), entityLocation.getLongitude())).title("Dirección de su entidad"));
+
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(entityLocation.getLatitude(), entityLocation.getLongitude(), 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(addresses.isEmpty()){
+            new AlertDialog.Builder(EntityRegisterActivity.this)
+                    .setTitle("Dirección")
+                    .setMessage("No se ha podido obtener la dirección de la entidad")
+                    .setPositiveButton("OK", null)
+                    .show();
+        } else {
+            moveToCurrentLocation(new LatLng(entityLocation.getLatitude(), entityLocation.getLongitude()));
+            textViewAddress.setText(addresses.get(0).getAddressLine(0));
+        }
+
+        mMap.setMinZoomPreference(10);
+        mMap.setMaxZoomPreference(20);
+
+
+        /* MAPS LISTENER */
+        // Click on the map
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                if(entityLocation == null){
+                    entityLocation = new Location("");
+                }
+
+                entityLocation.setLongitude(latLng.longitude);
+                entityLocation.setLatitude(latLng.latitude);
+
+                if(entityMarker == null){
+                    entityMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Dirección de su entidad"));
+                } else {
+                    entityMarker.setPosition(latLng);
+                }
+
+                List<Address> addresses = null;
+                try {
+                    addresses = geocoder.getFromLocation(entityLocation.getLatitude(), entityLocation.getLongitude(), 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if(addresses.isEmpty()){
+                    new AlertDialog.Builder(EntityRegisterActivity.this)
+                            .setTitle("Dirección")
+                            .setMessage("No se ha podido obtener la dirección de la entidad")
+                            .setPositiveButton("OK", null)
+                            .show();
+                } else {
+                    textViewAddress.setText(addresses.get(0).getAddressLine(0));
+                }
+            }
+        });
+
+        // Long click on the map
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                if(entityLocation == null){
+                    entityLocation = new Location("");
+                }
+
+                entityLocation.setLongitude(latLng.longitude);
+                entityLocation.setLatitude(latLng.latitude);
+
+                if(entityMarker == null){
+                    entityMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Dirección de su entidad"));
+                } else {
+                    entityMarker.setPosition(latLng);
+                }
+
+                List<Address> addresses = null;
+                try {
+                    addresses = geocoder.getFromLocation(entityLocation.getLatitude(), entityLocation.getLongitude(), 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if(addresses.isEmpty()){
+                    new AlertDialog.Builder(EntityRegisterActivity.this)
+                            .setTitle("Dirección")
+                            .setMessage("No se ha podido obtener la dirección de la entidad")
+                            .setPositiveButton("OK", null)
+                            .show();
+                } else {
+                    textViewAddress.setText(addresses.get(0).getAddressLine(0));
+                }
+            }
+        });
+    }
+
+    private void moveToCurrentLocation(LatLng currentLocation)
+    {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,15));
+        // Zoom in, animating the camera.
+        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+        // Zoom out to zoom level 10, animating with a duration of 2 seconds.
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+
+    }
+
 }
